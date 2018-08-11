e8e3a96e0d522da4aeea4360ee02fdc7bf4d22d0
leon-zheng <misterdayang@gmail.com>
2018-08-11 08:23:04 +0800

    fix time str length

--- get_time.m
+++ get_time.m
@@ -4,9 +4,21 @@ function time = get_time
     if size(m,2) == 1
         m = ['0',m];
     end
+    d = num2str(day(now));
+    if size(d,2) == 1
+        d = ['0',d];
+    end
+    h = num2str(hour(now));
+    if size(h,2) == 1
+        h = ['0',h];
+    end
+    min = num2str(minute(now));
+    if size(min,2) == 1
+        min = ['0',min];
+    end
     s = num2str(round(second(now)));
     if size(s,2) == 1
         s = ['0',s];
     end
-    time = [num2str(year(now)),m,num2str(day(now)),num2str(hour(now)),num2str(minute(now)),s];
+    time = [num2str(year(now)),m,d,h,min,s];
 end
